9a5474810e862cc1651f78c52338e2942e9a23d6
Shawn Miller (Mac) <shawn.a.miller@gmail.com>
2011-11-16 10:44:32 -0330

    Figured out getWorld(). Grep now displays world info.

--- GUI/src/com/emmz/grich/grep/grep.java
+++ GUI/src/com/emmz/grich/grep/grep.java
@@ -3,7 +3,6 @@ package com.emmz.grich.grep;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
-
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -13,6 +12,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class grep extends JavaPlugin {
+	private static final String String = null;
 	Logger log = Logger.getLogger("Minecraft");
 	
 	public void onEnable(){ 
@@ -25,20 +25,17 @@ public class grep extends JavaPlugin {
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-	    if(cmd.getName().equalsIgnoreCase("ignite")){
-			Player player =(Player) sender;
-			//if(args[1].equalsIgnoreCase("help")){
-			//	player.sendMessage(ChatColor.DARK_RED + "helpmenu");	
-			//	return true;
-			//} else {
-			//	player.sendRawMessage(server.getWorlds());
-			List<String> worldNames = new ArrayList<String>();
-			for(World w : this.getServer().getWorlds()) {
-			worldNames.add(w.getName());
-			}
-			player.sendRawMessage(worldNames);
+	    if(cmd.getName().equalsIgnoreCase("grep")){
+	    	Player player =(Player) sender;
+	    	World here = player.getWorld();
+			if(here.getName() == "CraftWorld{name=world_nether}"){
+				player.sendMessage(ChatColor.AQUA + "" + here.getName());
 				return true;
-			//}
+			} else {
+				player.sendMessage("Not sure where you are.");
+				return true;
+			}	
+
 	    }
 	    return false;
 	}	
